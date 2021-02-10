0092711ed1b481663f8275027d8d45289e470d05
frrf <franciscopali71@gmail.com>
2021-02-09 19:10:54 -0800

    fine tuned loading screen, need to add it to be active while tweets are loading after post request rendering of index.html

--- app/static/scripts/app.js
+++ app/static/scripts/app.js
@@ -7,3 +7,16 @@ function generateTweets () {
     twttr.widgets.createTweet(multipleIDS[i].textContent,tweetContainer);
   }
 }
+
+let btn = document.getElementById('btn');
+let loader = document.getElementsByClassName('loader')[0];
+btn.addEventListener('click', showLoading);
+
+function showLoading() {
+  let handleInput = document.getElementById('target_handle').value;
+  let searchInput = document.getElementById('term_to_search').value;
+  if (handleInput !== "" && searchInput !== "") {
+    loader.style.display = "flex";
+  } 
+}
+
